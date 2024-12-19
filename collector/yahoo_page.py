3b07345c4ec04c63bc4eb9b32fc24600b30f0b0d
msd-hrk <domorugan123@gmail.com>
2024-12-19 17:41:50 +0900

    header select removed at get_closing_price method in yahoo_page.py

--- collector/yahoo_page.py
+++ collector/yahoo_page.py
@@ -22,7 +22,7 @@ class YahooCollector():
         self.referenc_block = soup.select("#referenc")[0]
 
     def get_closing_price(self):
-        price = utils.del_str(self.realtime_block.select("header .StyledNumber__value__3rXW")[0].get_text(),",")
+        price = utils.del_str(self.realtime_block.select(".StyledNumber__value__3rXW")[0].get_text(),",")
         if not utils.int_check(price):
             return ""
         closing_price = str(utils.del_str(price, ","))
